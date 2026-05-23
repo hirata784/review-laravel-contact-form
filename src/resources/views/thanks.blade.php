1fc72adce722f005972801aee453bbf532330c40
hirata kyouko <tonkinese0chantilly.2615@gmail.com>
2026-05-23 20:42:40 +0900

    サンクスページ(thanks) UI 作成

--- src/resources/views/thanks.blade.php
+++ src/resources/views/thanks.blade.php
@@ -6,10 +6,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="{{asset('css/sanitize.css')}}">
+    <link rel="stylesheet" href="{{asset('css/thanks.css')}}">
 </head>
 
 <body>
-    サンクスページ
+    <div class="thanks">
+        <form action="/" class="front">
+            <p class="str">お問い合わせありがとうございました</p>
+            <button class="btn" type="submit">HOME</button>
+        </form>
+        <p class="back">Thank you</p>
+    </div>
 </body>
 
 </html>
