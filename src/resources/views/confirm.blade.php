f22cc8f47af696ff687af6b868f8ed8f44a46b5e
hirata kyouko <tonkinese0chantilly.2615@gmail.com>
2026-05-23 18:00:31 +0900

    確認ページ(confirm) UI 作成

--- src/resources/views/confirm.blade.php
+++ src/resources/views/confirm.blade.php
@@ -1,5 +1,51 @@
 @extends('layouts.app')
 
+@section('css')
+<link rel="stylesheet" href="{{asset('css/confirm.css')}}">
+@endsection
+
 @section('content')
-<div>確認ページ</div>
+<div class="confirm">
+    <h2 class="title">Confirm</h2>
+    <form action="">
+        <table>
+            <tr>
+                <th>お名前</th>
+                <td>テスト太郎</td>
+            </tr>
+            <tr>
+                <th>性別</th>
+                <td>テスト性別</td>
+            </tr>
+            <tr>
+                <th>メールアドレス</th>
+                <td>test@exmaple.com</td>
+            </tr>
+            <tr>
+                <th>電話番号</th>
+                <td>1112223333</td>
+            </tr>
+            <tr>
+                <th>住所</th>
+                <td>テスト県</td>
+            </tr>
+            <tr>
+                <th>建物名</th>
+                <td>テストアパート</td>
+            </tr>
+            <tr>
+                <th>お問い合わせの種類</th>
+                <td>テスト種類</td>
+            </tr>
+            <tr>
+                <th>お問い合わせ内容</th>
+                <td>テスト内容</td>
+            </tr>
+        </table>
+        <div class="group-btn">
+            <button type="submit" class="btn">送信</button>
+            <a class="correction" href="">修正</a>
+        </div>
+    </form>
+</div>
 @endsection
