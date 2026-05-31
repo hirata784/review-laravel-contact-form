819c9d76e8d73097a6e1058f4747effdea395ed4
hirata kyouko <tonkinese0chantilly.2615@gmail.com>
2026-05-31 19:17:22 +0900

    登録ページ(register) UI 作成

--- src/resources/views/auth/register.blade.php
+++ src/resources/views/auth/register.blade.php
@@ -1,5 +1,30 @@
 @extends('layouts.app')
 
+@section('css')
+<link rel="stylesheet" href="{{asset('css/register.css')}}">
+@endsection
+
 @section('content')
-<div>登録画面</div>
+<div class="register">
+    <div class="register-content">
+        <h2 class="title">Register</h2>
+        <form class="register-form" action="">
+            <div class="register-txt-group">
+                <div class="group">
+                    <p class="group-item">お名前</p>
+                    <input type="text" class="txt" placeholder="例: 山田 太郎">
+                </div>
+                <div class="group">
+                    <p class="group-item">メールアドレス</p>
+                    <input type="text" class="txt" placeholder="例:test@example.com">
+                </div>
+                <div class="group">
+                    <p class="group-item">パスワード</p>
+                    <input type="text" class="txt" placeholder="例:coachtech1106">
+                </div>
+            </div>
+            <button class="btn">登録</button>
+        </form>
+    </div>
+</div>
 @endsection
