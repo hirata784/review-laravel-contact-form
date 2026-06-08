9b4d473bbe18777b4c1819ba5cd9433d9cc79a9c
hirata kyouko <tonkinese0chantilly.2615@gmail.com>
2026-06-08 14:07:16 +0900

    管理画面(admin) ページネーション表示で詳細機能が使えるように修正

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -15,21 +15,30 @@ const tdDetail = document.getElementById("td-detail");
 // 詳細クリックイベント
 detailBtn.forEach((button) => {
     button.addEventListener("click", function () {
-        // 詳細データ取得
-        let targetId = this.dataset.target;
+        // 現在のURLのパラメータを取得
+        const urlParams = new URLSearchParams(window.location.search);
+        // "page" という名前のパラメータを取得（例: ?page=3 なら "3"）
+        let currentPageStr = urlParams.get("page");
+        // currentPageStrが空白の場合、1を取得
+        currentPageStr = currentPageStr == null ? 1 : currentPageStr;
+
+        // 詳細データ取得(クリックした行 + (7 * (現在のページ - 1)))
+        const targetId =
+            parseInt(this.dataset.target) + parseInt(7 * (currentPageStr - 1));
+
         tdName.textContent =
-            tableData[targetId]["last_name"] +
+            tableData.data[targetId]["last_name"] +
             " " +
-            tableData[targetId]["first_name"];
-        tdGender.textContent = tableData[targetId]["gender"];
-        tdEmail.textContent = tableData[targetId]["email"];
-        tdTel.textContent = tableData[targetId]["tel"];
-        tdAddress.textContent = tableData[targetId]["address"];
-        tdBuilding.textContent = tableData[targetId]["building"];
-        tdCategory.textContent = tableData[targetId]["category"];
-        tdDetail.textContent = tableData[targetId]["detail"];
+            tableData.data[targetId]["first_name"];
+        tdGender.textContent = tableData.data[targetId]["gender"];
+        tdEmail.textContent = tableData.data[targetId]["email"];
+        tdTel.textContent = tableData.data[targetId]["tel"];
+        tdAddress.textContent = tableData.data[targetId]["address"];
+        tdBuilding.textContent = tableData.data[targetId]["building"];
+        tdCategory.textContent = tableData.data[targetId]["category"];
+        tdDetail.textContent = tableData.data[targetId]["detail"];
         // データ削除用idをhiddenで作成
-        hidden.value = tableData[targetId]["id"];
+        hidden.value = tableData.data[targetId]["id"];
         // モーダル表示
         modal.style.display = "block";
     });
